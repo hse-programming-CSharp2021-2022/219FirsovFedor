84149c6892b9d59d335d0c7d79a768794353c017
Федор Фирсов <fafirsov@edu.hse.ru>
2022-04-08 09:50:22 +0300

    _

---
+++ 3_mod/test/test/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace test
+{
+    static struct A { }
+    class Program
+    {
+        static void Main()
+        {
+            data m = (data)5;
+            Console.WriteLine(m);
+        }
+    }
+   
+
+
+}
---
+++ 4_mod/CW_01/cmake-build-debug/CMakeFiles/CW_01.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CW_01.dir/main.cpp.obj"
+  "CW_01.exe"
+  "CW_01.exe.manifest"
+  "CW_01.pdb"
+  "libCW_01.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CW_01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
